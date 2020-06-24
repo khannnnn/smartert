1bf921c500cc0e221d275c9c07056bc80c9677f2
Irfan <irfanbtp786@gmail.com>
2020-06-24 13:05:22 +0530

    Read me file changes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# smartert
+1. Select path for cloning project.
+2. Open CMD window follow below steps.
+    A. Go Folder location.
+    B. Type command on CMD "git clone https://github.com/khannnnn/smartert.git"
+    C. Go inside project using "cd smartert"
+    D. Install all packeges using "npm i"
+    E. Type command for JSON server start "npm install -g json-server"
+    F. After installed JSON server type command  "json-server --watch db.json"
+    G. Server started.
+    H. Open other CMD window type command "npm start"
+    I. Now you can check your browser.
+3. If still you are faching any problem please call me on 7740878891.
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -20,27 +20,27 @@ class Home extends React.Component {
     }
 
     callbackFunctionHeader = (childData) => {
-        if(childData != ''){
+        if (childData != '') {
             this.searchPost(childData);
         } else {
             this.submitPost();
         }
     }
 
-    createPost(){
+    createPost() {
         this.setState({
             createPost: !this.state.createPost
         })
     }
 
-    postList(){
+    postList() {
         this.setState({
             postList: !this.state.postList
         })
     }
 
-    componentDidMount(){
-        if(this.props.postListData.length > 0){
+    componentDidMount() {
+        if (this.props.postListData.length > 0) {
             this.setState({
                 allPost: this.props.postListData
             })
@@ -49,15 +49,15 @@ class Home extends React.Component {
         }
     }
 
-    searchPost(childData){
-        let url = 'http://localhost:3000/posts?title='+childData;
+    searchPost(childData) {
+        let url = 'http://localhost:3000/posts?title=' + childData;
         fetch(url, {
             method: "GET",
             headers: { 'Content-Type': 'application/json' },
         }).then((res => {
             res.json().then((data) => {
                 console.log("response", data);
-                if(data){
+                if (data) {
                     this.setState({
                         allPost: data
                     })
@@ -67,7 +67,7 @@ class Home extends React.Component {
         }))
     }
 
-    submitPost(){
+    submitPost() {
         let url = 'http://localhost:3000/posts';
         fetch(url, {
             method: "GET",
@@ -75,7 +75,7 @@ class Home extends React.Component {
         }).then((res => {
             res.json().then((data) => {
                 console.log("response", data);
-                if(data){
+                if (data) {
                     this.setState({
                         allPost: data
                     })
@@ -88,19 +88,19 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <Header parentCallbackHeader = {this.callbackFunctionHeader}/>
+                <Header parentCallbackHeader={this.callbackFunctionHeader} />
                 <div className="post">
                     <div className="post-main">
                         <div className="center-div">
                             <button className="form-field"
-                                onClick={()=>this.createPost()}
+                                onClick={() => this.createPost()}
                             >
                                 New Post</button>
                         </div>
                         <div className="center-div">
                             <button
                                 className="form-field"
-                                onClick={()=>this.postList()}
+                                onClick={() => this.postList()}
                             >Published</button>
                         </div>
                     </div>
@@ -108,16 +108,16 @@ class Home extends React.Component {
                 <div className="main-home" >
                     <div className="sub-home">
                         <div className="left-div" style={{ paddingRight: "20px" }}>
-                        {
-                            this.state.createPost ? 
-                                <NewPost parentCallback = {this.callbackFunction} />
-                            : null
-                        }
-                            
+                            {
+                                this.state.createPost ?
+                                    <NewPost parentCallback={this.callbackFunction} />
+                                    : null
+                            }
+
                         </div>
                         <div className="left-div" style={{ paddingLeft: "20px" }}>
                             {
-                                this.state.postList ? 
+                                this.state.postList ?
                                     <div>
                                         <table className="tbody">
                                             <thead>
@@ -137,12 +137,15 @@ class Home extends React.Component {
                                                                 <td>{item.post}</td>
                                                             </tr>
                                                         )
-                                                    : null
+                                                        :
+                                                        <tr>
+                                                            <td colSpan="3">No Record found..</td>
+                                                        </tr>
                                                 }
                                             </tbody>
                                         </table>
                                     </div>
-                                : null
+                                    : null
                             }
                         </div>
                     </div>
@@ -156,7 +159,7 @@ const mapDispatchToProps = dispatch => ({
     postListAction: (data) => dispatch(actions.postList(data))
 })
 
-const mapStateToProps = state =>({
+const mapStateToProps = state => ({
     postListData: state.postList
 })
 
